d6db61be8ff689243a6c2fe86d177c0be6576828
Thota-bharat999 <bharatmanithota@gmail.com>
2026-01-20 16:43:56 +0530

    cors error

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,61 +1,74 @@
-const socket=require("socket.io");
-const crypto=require("crypto");
+const socket = require("socket.io");
+const crypto = require("crypto");
 const { Chat } = require("../models/chat");
 
-const getScretRoomId=(userId,targetUserId)=>{
-   return crypto.createHash("sha256").update([userId,targetUserId].sort().join("_")).digest("hex")
-}
+const getScretRoomId = (userId, targetUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("_"))
+    .digest("hex");
+};
 
-const initilizeSocket=(server)=>{
-    const io=socket(server,{
-    cors:{
-        origin:"http://localhost:5173",
+const initilizeSocket = (server) => {
+  const io = socket(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+      credentials: true,
     },
-});
-io.on("connection",(socket)=>{
-    socket.on("joinChat",({firstName,userId,targetUserId,text})=>{
-        const roomId=getScretRoomId(userId,targetUserId)
-        console.log(firstName+ "joing Room :" +roomId)
-        socket.join(roomId)
-    
-    })
-    socket.on("sendMessage", async({
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Socket connected:", socket.id);
+
+    socket.on("joinChat", ({ firstName, lastName, userId, targetUserId }) => {
+      const roomId = getScretRoomId(userId, targetUserId);
+      console.log(firstName + " joining Room : " + roomId);
+      socket.join(roomId);
+    });
+
+    socket.on(
+      "sendMessage",
+      async ({ firstName, lastName, userId, targetUserId, text }) => {
+        try {
+          const roomId = getScretRoomId(userId, targetUserId);
+          console.log(firstName + " : " + text);
+
+          let chat = await Chat.findOne({
+            participantes: { $all: [userId, targetUserId] },
+          });
+
+          if (!chat) {
+            chat = new Chat({
+              participantes: [userId, targetUserId],
+              messages: [],
+            });
+          }
+
+          chat.messages.push({
+            senderId: userId,
+            text,
+          });
+
+          await chat.save();
+
+          // IMPORTANT: send senderId to frontend
+          io.to(roomId).emit("messageRecived", {
+            senderId: userId,
             firstName,
             lastName,
-            userId,
-            targetUserId,
-            text
-        })=>{
-          
-             try{
-                 const roomId=getScretRoomId(userId,targetUserId)
-             console.log(firstName+ " "+ text)
-                let chat = await Chat.findOne({
-                    participantes:{$all:[userId,targetUserId]},
-                })
-                if(!chat){
-                    chat=new Chat({
-                        participantes:[userId,targetUserId],
-                        messages:[],
-                    });
-                }
-              chat.messages.push({
-                senderId:userId,
-                text,
-              });
-              await chat.save()
-              io.to(roomId).emit("messageRecived",{firstName,lastName,text})
+            text,
+          });
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    );
 
-             }catch(err){
-                console.error(err)
-             }
-             
-    
-    })
-    socket.on("disConnect",()=>{
-    
-    })
-})
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected:", socket.id);
+    });
+  });
+};
 
-}
-module.exports=initilizeSocket
+module.exports = initilizeSocket;
